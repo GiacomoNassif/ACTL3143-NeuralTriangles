8c93c948b9f7913221345843e07240d189ba35b6
Jack.Nassif <Jack.Nassif@Finity.com.au>
2022-07-21 19:45:26 +1000

    Refactored
    Refactored and added a company lookup

--- deeptriangle.py
+++ deeptriangle.py
@@ -1,59 +1,59 @@
 import keras
+import numpy as np
 from keras.layers import Embedding, Masking, GRU, Flatten, RepeatVector, Concatenate, TimeDistributed, Dense, \
-    Dropout
+    Dropout, IntegerLookup
 
-company_code = keras.Input(
-    shape=(1,),
-    name='company_code',
-    dtype='int32'
-)
+def get_deep_triangle(company_codes: np.ndarray):
+    company_code = keras.Input(
+        shape=(1,),
+        name='company_code',
+        dtype='int32'
+    )
 
-company_code_embedding = Embedding(
-    input_dim=200, output_dim=49, name='company_code_embedding'
-)(company_code)
+    company_code_mapping = IntegerLookup()
+    company_code_mapping.adapt(company_codes)
 
-company_code_embedding = Flatten()(company_code_embedding)
-company_code_embedding = RepeatVector(9)(company_code_embedding)
+    company_code_mapped = company_code_mapping(company_code)
 
-loss_paid = keras.Input(
-    shape=(9, 1),
-    name='Incremental_Loss_Paid_Loss_Ratio'
-)
+    company_code_embedding = Embedding(
+        input_dim=200, output_dim=49, name='company_code_embedding'
+    )(company_code_mapped)
 
-case_reserves = keras.Input(
-    shape=(9, 1),
-    name='Case_Reserves_Loss_Ratio'
-)
+    company_code_embedding = Flatten()(company_code_embedding)
+    company_code_embedding = RepeatVector(9)(company_code_embedding)
 
-# ay_input = keras.Input(
-#     shape=(9,2),
-#     name='ay_input'
-# )
+    loss_paid = keras.Input(
+        shape=(9, 1),
+        name='Incremental_Loss_Paid_Loss_Ratio'
+    )
 
-ay_input = Concatenate(axis=2)([loss_paid, case_reserves])
+    case_reserves = keras.Input(
+        shape=(9, 1),
+        name='Case_Reserves_Loss_Ratio'
+    )
 
-encoded = Masking(mask_value=-99)(ay_input)
-encoded = GRU(units=128, dropout=0.2, recurrent_dropout=0.2)(encoded)
+    ay_input = Concatenate(axis=2)([loss_paid, case_reserves])
 
-decoded = RepeatVector(n=9)(encoded)
-decoded = GRU(units=128, return_sequences=True, dropout=0.2, recurrent_dropout=0.2)(decoded)
+    encoded = Masking(mask_value=-99)(ay_input)
+    encoded = GRU(units=128, dropout=0.2, recurrent_dropout=0.2)(encoded)
 
-history_and_company_code = Concatenate(axis=2)([decoded, company_code_embedding])
+    decoded = RepeatVector(n=9)(encoded)
+    decoded = GRU(units=128, return_sequences=True, dropout=0.2, recurrent_dropout=0.2)(decoded)
 
-case_reserves_output = TimeDistributed(Dense(units=64, activation='relu'))(history_and_company_code)
-case_reserves_output = TimeDistributed(Dropout(rate=0.2))(case_reserves_output)
-case_reserves_output = TimeDistributed(
-    Dense(units=1, activation='relu'),
-    name='Case_Reserves_Output')(
-    case_reserves_output
-)
+    history_and_company_code = Concatenate(axis=2)([decoded, company_code_embedding])
 
-paid_output = TimeDistributed(Dense(units=64, activation='relu'))(decoded)
-paid_output = TimeDistributed(Dropout(rate=0.2))(paid_output)
-paid_output = TimeDistributed(Dense(units=1, activation='relu'), name='Paid_Output')(paid_output)
+    case_reserves_output = TimeDistributed(Dense(units=64, activation='relu'))(history_and_company_code)
+    case_reserves_output = TimeDistributed(Dropout(rate=0.2))(case_reserves_output)
+    case_reserves_output = TimeDistributed(
+        Dense(units=1, activation='relu'),
+        name='Case_Reserves_Output')(
+        case_reserves_output
+    )
 
+    paid_output = TimeDistributed(Dense(units=64, activation='relu'))(decoded)
+    paid_output = TimeDistributed(Dropout(rate=0.2))(paid_output)
+    paid_output = TimeDistributed(Dense(units=1, activation='relu'), name='Paid_Output')(paid_output)
 
-def get_deep_triangle():
     return keras.Model(
         inputs=[company_code, loss_paid, case_reserves],
         outputs=[paid_output, case_reserves_output]
