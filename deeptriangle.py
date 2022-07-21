d7d63db13b67328f29b2b4408c53dc58a4ecab28
Jack.Nassif <Jack.Nassif@Finity.com.au>
2022-07-22 01:07:09 +1000

    Update deeptriangle.py
    Added in masked loss function

--- deeptriangle.py
+++ deeptriangle.py
@@ -59,3 +59,34 @@ def get_deep_triangle(company_codes: np.ndarray):
         inputs=[company_code, loss_paid, case_reserves],
         outputs=[paid_output, case_reserves_output]
     )
+
+
+import keras.backend as K
+
+"""
+Source: https://stackoverflow.com/a/70126561/10916209
+"""
+
+
+def get_loss(mask_value):
+    mask_value = K.variable(mask_value)
+
+    def masked_loss(yTrue, yPred):
+        # find which values in yTrue (target) are the mask value
+        isMask = K.equal(yTrue, mask_value)  # true for all mask values
+
+        # transform to float (0 or 1) and invert
+        isMask = K.cast(isMask, dtype=K.floatx())
+        isMask = 1 - isMask  # now mask values are zero, and others are 1
+
+        # multiply this by the inputs:
+        # maybe you might need K.expand_dims(isMask) to add the extra dimension removed by K.all
+        yTrue = yTrue * isMask
+        yPred = yPred * isMask
+
+        # perform a root mean square error, whereas the mean is in respect to the mask
+        mean_loss = K.sum(K.square(yPred - yTrue)) / K.sum(isMask)
+
+        return mean_loss
+
+    return masked_loss
