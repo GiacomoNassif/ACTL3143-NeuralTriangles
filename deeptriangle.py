4e5bbaf4c86aca5899304c5afa5534037b26f106
Jack.Nassif <Jack.Nassif@Finity.com.au>
2022-07-21 17:15:21 +1000

    Cleaned up deep triangle code.

--- deeptriangle.py
+++ deeptriangle.py
@@ -1,12 +1,11 @@
 import keras
-from keras.layers import Embedding, Masking, GRU, Flatten, RepeatVector, Lambda, Concatenate, TimeDistributed, Dense, \
+from keras.layers import Embedding, Masking, GRU, Flatten, RepeatVector, Concatenate, TimeDistributed, Dense, \
     Dropout
-import tensorflow as tf
-import numpy as np
 
 company_code = keras.Input(
     shape=(1,),
-    name='company_code'
+    name='company_code',
+    dtype='int32'
 )
 
 company_code_embedding = Embedding(
@@ -26,6 +25,11 @@ case_reserves = keras.Input(
     name='Case_Reserves_Loss_Ratio'
 )
 
+# ay_input = keras.Input(
+#     shape=(9,2),
+#     name='ay_input'
+# )
+
 ay_input = Concatenate(axis=2)([loss_paid, case_reserves])
 
 encoded = Masking(mask_value=-99)(ay_input)
@@ -33,9 +37,10 @@ encoded = GRU(units=128, dropout=0.2, recurrent_dropout=0.2)(encoded)
 
 decoded = RepeatVector(n=9)(encoded)
 decoded = GRU(units=128, return_sequences=True, dropout=0.2, recurrent_dropout=0.2)(decoded)
-decoded = Concatenate()([decoded, company_code_embedding])
 
-case_reserves_output = TimeDistributed(Dense(units=64, activation='relu'))(decoded)
+history_and_company_code = Concatenate(axis=2)([decoded, company_code_embedding])
+
+case_reserves_output = TimeDistributed(Dense(units=64, activation='relu'))(history_and_company_code)
 case_reserves_output = TimeDistributed(Dropout(rate=0.2))(case_reserves_output)
 case_reserves_output = TimeDistributed(
     Dense(units=1, activation='relu'),
@@ -47,21 +52,9 @@ paid_output = TimeDistributed(Dense(units=64, activation='relu'))(decoded)
 paid_output = TimeDistributed(Dropout(rate=0.2))(paid_output)
 paid_output = TimeDistributed(Dense(units=1, activation='relu'), name='Paid_Output')(paid_output)
 
-model = keras.Model(
-    inputs=[company_code, loss_paid, case_reserves],
-    outputs=[paid_output, case_reserves_output]
-)
 
-model.compile(
-    optimizer='adam',
-    loss='mse',
-    loss_weights=[0.5, 0.5]
-)
-
-model.fit(
-    x=training_data[:, :3],
-    y=training_data[:, -2:],
-    validation_data=(validation_data[:, :3], validation_data[:, -3:]),
-    batch_size=
-
-)
+def get_deep_triangle():
+    return keras.Model(
+        inputs=[company_code, loss_paid, case_reserves],
+        outputs=[paid_output, case_reserves_output]
+    )
